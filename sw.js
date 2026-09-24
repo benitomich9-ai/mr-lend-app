2c9d222e1d0a35be8234ffbbe721d4f94a6feaba
benitomich9-ai <benitomich9@gmail.com>
2026-09-24 11:33:07 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'mr-lends-cache-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
